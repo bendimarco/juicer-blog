397b973ccecb09eb551288bbb69d356844baf7ae
Adit Bala <adityarbala@gmail.com>
2022-01-08 15:18:58 -0800

    add Article Buffer to finish API call before rendering Articles

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,6 +4,7 @@ import PreviewPost from "./PreviewPost";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "../styles/MainPage.css";
 import Nav from "./Nav";
+import ArticleBuffer from "./ArticleBuffer";
 
 export default function GetInfo() {
   const [articleList, setArticles] = useState([]);
@@ -43,37 +44,11 @@ export default function GetInfo() {
     setInactiveNavAuthor(tempAuthor);
   }
 
-
-
   return (
-    <>
-      <div className="page-content">
-        <Nav
-          author={navAuthor}
-          switchAuthor={switchAuthors}
-          articlePage={false}
-        />
-        <div className="article-list-container">
-          {articleList.map((a) => (
-            <div key={a.id.toString()}>
-              <PreviewPost
-                key={a.attributes.slug.toString()}
-                article={a.attributes}
-                author={navAuthor}
-              ></PreviewPost>
-              <div className="read-btn-container">
-                <Link
-                  className="read-btn"
-                  key={a.id.toString()}
-                  to={`${process.env.PUBLIC_URL}/${a.attributes.slug}`}
-                >
-                  Read More
-                </Link>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
+    <ArticleBuffer
+      navAuthor={navAuthor}
+      articleList={articleList}
+      switchAuthors={switchAuthors}
+    />
   );
 }
